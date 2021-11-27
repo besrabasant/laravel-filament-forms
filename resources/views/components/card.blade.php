e4254bbb24718edc6a97bfc714567faf340a47a5
Basant Besra <besrabasant@gmail.com>
2021-11-27 22:24:39 +0530

    Add label support to card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,18 @@
+@php
+    /**
+     * @var Closure $getLabel
+     */
+    $label = $getLabel();
+@endphp
 <div
     {!! $getId() ? "id=\"{$getId()}\"" : null !!}
-    {{ $attributes->merge($getExtraAttributes())->class(['p-6 bg-white shadow rounded-xl', 'filament-forms-card-component']) }}
+    {{ $attributes->merge($getExtraAttributes())->class(['p-6 bg-white shadow rounded-md', 'filament-forms-card-component']) }}
 >
+    @if($label && !$isLabelHidden())
+        <div class="mb-4 text-xl font-medium text-gray-500">
+            {!! $getLabel() !!}
+        </div>
+    @endif
+
     {{ $getChildComponentContainer() }}
 </div>
